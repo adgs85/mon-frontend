9d540d07c784d6c6e6cfebc0a9026dd262606a9a
adgs <dummy>
2022-03-17 17:24:26 +0200

    initial home page + added url-parse package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Comp1 } from './components/test/Comp1';
 import { HomePage } from './components/main/HomePage';
 
 function App() {
--- src/components/main/HomePage.tsx
+++ src/components/main/HomePage.tsx
@@ -1,10 +1,56 @@
 import React from "react";
 
 
-export class HomePage extends React.PureComponent<any, {date: Date}> {
+
+const defaultServerUrl="http://localhost:8080"
+
+export interface HomePageState{
+    serverUrl: string | undefined
+}
+
+export interface HomePageProps{
+
+}
+
+export class HomePage extends React.PureComponent<HomePageProps, HomePageState> {
+
+    serverUrlInternal=defaultServerUrl
+
+    serverUrinputRef = React.createRef<HTMLInputElement>()
+
+    constructor(props: HomePageProps){
+        super(props)
+        this.state = {
+            serverUrl: defaultServerUrl
+        }
+    }
+
+    applyServerUrlInternal(){
+        this.setState({
+            serverUrl: this.serverUrinputRef.current?.value
+        })
+    }
+
+
     render (){
-        return <div>
-            HOME PAGE
-        </div>
+        return <div className="container">
+                    <p>
+                        <div className="row align-items-start" style={{borderStyle: 'groove'}}>
+                            <div className="col-6">
+                                <input defaultValue={defaultServerUrl} ref={this.serverUrinputRef}></input>
+                                <button onClick={this.applyServerUrlInternal}>Apply</button>
+                                <span className="alert alert-danger">Invalid</span>
+                            </div>
+                        </div>
+                    </p>
+                    <p>
+                    <div className="row align-items-start" style={{borderStyle: 'groove'}}>
+                        <div className="col-1">
+                            One of three columns
+                        </div>
+                        <div className="col-11" style={{borderStyle: 'groove'}}> qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd </div>
+                    </div>
+                    </p>
+            </div>
     }
 }
