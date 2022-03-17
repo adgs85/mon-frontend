100ba43779c7051783017ffbe7043b3fed914215
adgs <dummy>
2022-03-17 17:53:38 +0200

    Nav bar with active tab highlight, currently does nothing

---
+++ src/components/main/NavBar.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+
+export interface NavBarProps {
+
+}
+
+export function NavBar(props: NavBarProps) {
+
+    const [activeTabIdx, setActiveTabIdx] = useState(0);
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a href="/" className="navbar-brand">Agents Monitor</a>
+                <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className="navbar-nav">
+
+                        <span
+                            className={"nav-item nav-link" + (activeTabIdx === 0 ? " active" : "")}
+                            onClick={() => setActiveTabIdx(0)}>Home</span>
+
+                        <span
+                            className={"nav-item nav-link" + (activeTabIdx === 1 ? " active" : "")}
+                            onClick={() => setActiveTabIdx(1)}>Settings (todo)</span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
