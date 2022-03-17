744da68e5b145a767b7fb4b8743e1477b5babcb8
adgs <dummy>
2022-03-17 17:54:20 +0200

    switched to fluid container and added nav bar component

--- src/components/main/HomePage.tsx
+++ src/components/main/HomePage.tsx
@@ -1,41 +1,47 @@
 import React from "react";
+import { NavBar } from "./NavBar";
 
 
 
-const defaultServerUrl="http://localhost:8080"
+const defaultServerUrl = "http://localhost:8080"
 
-export interface HomePageState{
+export interface HomePageState {
     serverUrl: string | undefined
 }
 
-export interface HomePageProps{
+export interface HomePageProps {
 
 }
 
 export class HomePage extends React.PureComponent<HomePageProps, HomePageState> {
 
-    serverUrlInternal=defaultServerUrl
+    serverUrlInternal = defaultServerUrl
 
     serverUrinputRef = React.createRef<HTMLInputElement>()
 
-    constructor(props: HomePageProps){
+    constructor(props: HomePageProps) {
         super(props)
         this.state = {
             serverUrl: defaultServerUrl
         }
     }
 
-    applyServerUrlInternal(){
+    applyServerUrlInternal() {
         this.setState({
             serverUrl: this.serverUrinputRef.current?.value
         })
     }
 
 
-    render (){
-        return <div className="container">
+    render() {
+        return <div>
+
+            <NavBar/>
+            
+            <div className="p-5 mlr-4 bg-light rounded-3">
+                <div className="container-fluid">
                     <p>
-                        <div className="row align-items-start" style={{borderStyle: 'groove'}}>
+                        <div className="row align-items-start" style={{ borderStyle: 'groove' }}>
                             <div className="col-6">
                                 <input defaultValue={defaultServerUrl} ref={this.serverUrinputRef}></input>
                                 <button onClick={this.applyServerUrlInternal}>Apply</button>
@@ -44,13 +50,15 @@ export class HomePage extends React.PureComponent<HomePageProps, HomePageState> 
                         </div>
                     </p>
                     <p>
-                    <div className="row align-items-start" style={{borderStyle: 'groove'}}>
-                        <div className="col-1">
-                            One of three columns
+                        <div className="row align-items-start" style={{ borderStyle: 'groove' }}>
+                            <div className="col-1">
+                                One of three columns
+                            </div>
+                            <div className="col-11" style={{ borderStyle: 'groove' }}> qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd </div>
                         </div>
-                        <div className="col-11" style={{borderStyle: 'groove'}}> qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd qwdqwd </div>
-                    </div>
                     </p>
+                </div>
             </div>
+        </div>
     }
 }
