175fb8aa84c75414566575f70463a957d29c2ffe
adgs <dummy>
2022-03-18 18:50:33 +0200

    removed home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import { Layout } from './components/layout/Layout';
-import { HomePage } from './components/main/HomePage';
 
 function App() {
   return (
     <div>
-      <Layout>
-        <HomePage/>
-      </Layout>
+      <Layout/>
     </div>
   );
 }
